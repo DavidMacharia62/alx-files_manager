2fefc1c266036ad22165bdbd8568bb73ea7977b6
DavidMacharia62 <davidmacharia1@gmail.com>
2024-02-13 04:08:28 -0800

    db.js file added

---
+++ utils/db.js
@@ -0,0 +1,35 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || '27017';
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${this.host}:${this.port}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(`${this.database}`);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const collection = this.db.collection('users');
+    const result = await collection.countDocuments();
+    return result;
+  }
+
+  async nbFiles() {
+    const collection = this.db.collection('files');
+    const result = await collection.countDocuments();
+    return result;
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
